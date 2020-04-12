3e2fd49ac993fa0c39761b5135d68db41e85e489
Shawn Cavalieri <shawn.cavalieri@yahoo.com>
2020-04-12 15:33:16 -0400

    final changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,58 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+// load data into the table
+
+function loadData(data) {
+    tbody.text("")
+
+    data.forEach(function(ufosightings) {
+        console.log(ufosightings);
+        var row = tbody.append("tr");
+        Object.entries(ufosightings).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+loadData(tableData);
+
+// filter table
+
+var filter = d3.select("#filter-btn");
+
+var reset = d3.select("#reset-btn");
+
+filter.on("click", function() {
+  
+  d3.event.preventDefault();
+  var inputDate = d3.select("#datetime").property("value");  
+  console.log(inputDate);
+
+  var filteredData = data.filter(bydate => bydate.datetime === inputDate);
+  console.log(filteredData)
+
+  filteredData.forEach(function(updated) {
+    console.log(updated);
+    var row = tbody.append("tr");
+
+
+    Object.entries(updated).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+});
+
+// reset table
+reset.on("click", function() {
+    loadData(tableData)
+    d3.select("#datetime").property("value", '');
+  });
